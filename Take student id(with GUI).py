19836b7adb3b36183b3b7981bbaed499c3d1e418
buxiuzhixing <484001467@qq.com>
2024-02-16 16:59:18 +0800

    修复BUG

--- Take student id(with GUI).py
+++ Take student id(with GUI).py
@@ -22,7 +22,7 @@ def generate_number():
 def delete():
     o.system("cls")
 def about():
-    print("="*128,"\n关于：本程序基本框架由ChatGPT生成，使用Python编写\n算法实现：本程序随机数是使用Python原生库Ramdom通过特定的算法生成的。\n请注意：完全的随机性是不存在于世上的。若您对结果存在质疑或不满，那纯属正常。\n\n下载最新版：\n码云：https://gitee.com/seedream7649/basic-utility-applet\nGitHub：https://github.com/buxiuzhixing/basic-utility-applet\n","="*128)
+    print("="*128,"\n关于：本程序基本框架由ChatGPT生成，使用Python编写\n算法实现：本程序随机数是使用Python原生库Ramdom通过特定的算法生成的。\n请注意：完全的随机性是不存在于世上的。若您对结果存在质疑或不满，那纯属正常。\n\n下载最新版：\n码云：https://gitee.com/seedream7649/basic-utility-applet/blob/master/抽学号GUI.py\nGitHub：https://github.com/buxiuzhixing/Basic-utility-applets/blob/main/Take student id(with GUI).py\n","="*128)
 # 创建一个按钮
 print("开始创建一个按钮……")
 button=tk.Button(root, text="抽取",font=("Helvetica",20), command=generate_number)
